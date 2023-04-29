05c86111c2f70367fb417e50b777b271c305a0ca
yourfriendoss <81387799+yourfriendoss@users.noreply.github.com>
2023-04-29 13:43:35 +0300

    [fix] Run on a unlikely to be opened port

--- main.ts
+++ main.ts
@@ -10,4 +10,4 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twindv1.ts";
 import twindConfig from "./twind.config.ts";
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, { plugins: [twindPlugin(twindConfig)], port: 4201 });
