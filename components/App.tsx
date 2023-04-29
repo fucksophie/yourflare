7bf30153c98b1a960cf729f9a5806e849df38f8e
yourfriendoss <81387799+yourfriendoss@users.noreply.github.com>
2023-04-29 17:38:45 +0300

    [chore] Add meta tags

--- components/App.tsx
+++ components/App.tsx
@@ -6,7 +6,20 @@ export function App(props: JSX.HTMLAttributes) {
     <>
       <head>
         <title>yourflare</title>
+        <meta name="title" content="yourflare"></meta>
+        <meta name="description" content="Opensource DNS managment."></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:title" content="yourflare"></meta>
+        <meta property="og:description" content="Opensource DNS managment."></meta>
+
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:title" content="yourflare"></meta>
+        <meta property="twitter:description" content="Opensource DNS managment."></meta>
+
         <link rel="stylesheet" href={asset("toastify.css")} />
+
       </head>
       <body>
         <div class="ml-2 mt-2">
