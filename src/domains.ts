38062dc1d0e0c135a25d0779bd665b12e9e2c799
yourfriendoss <81387799+yourfriendoss@users.noreply.github.com>
2023-05-01 15:01:55 +0300

    [fix] Was missing a dash here

--- src/domains.ts
+++ src/domains.ts
@@ -3,7 +3,7 @@ import { validationError } from './validation.ts';
 
 const asciiOnly = /^[a-zA-Z.0-9_*-]+$/;
 // const simpleDomainRegex = /^[a-zA-Z0-9_][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/gm
-const domainWithSubdomainRegex = /^[[a-zA-Z0-9_.]{1,61}\.(.+)$/gm
+const domainWithSubdomainRegex = /^[[a-zA-Z0-9_.-]{1,61}\.(.+)$/gm
 
 const Name = z.string().max(255).min(1).regex(asciiOnly);
 const Host = z.string().regex(domainWithSubdomainRegex);
